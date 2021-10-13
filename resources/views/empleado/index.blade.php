935237da11cb255c532a790cddfe8013aa17deb7
Julio Gonzalez <julioagonzalez18@gmail.com>
2021-10-13 13:36:39 -0400

    feat: Update views, include photo logic and eliminate employee

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -10,6 +10,28 @@
         </button>      
     </div>
     @endif 
-    <h2>Perfil de Usuario</h2>
+    
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <div class="card" style="width: 21rem;">
+            <img src="{{ asset('storage').'/'.$empleado->photo }}" class="card-img-top" alt="">
+            <div class="card-body">
+              <h4 class="card-title">{{ $empleado->name }} {{ $empleado->last_name }}</h4>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">{{ $empleado->email }}</li>
+              <li class="list-group-item">{{ $empleado->state }} {{ $empleado->city }}</li>
+              <li class="list-group-item">{{ $empleado->birth_date }}</li>
+              @if ($empleado->is_admin == 0)
+                <li class="list-group-item">Range: Employee</li>
+              @else
+                <li class="list-group-item">Range: Admin</li>
+              @endif              
+            </ul>            
+          </div>
+        </div>
+      </div>
+    </div>  
   </div>
 @endsection
